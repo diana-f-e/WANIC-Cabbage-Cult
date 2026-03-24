5f71c68590301f7ceff47a1355c4cf9ef7f72a2d
Diana <144166059+diana-f-e@users.noreply.github.com>
2026-03-24 08:13:51 -0700

    SO stuff

---
+++ Cabbage Cult/Assets/Scripts/Scriiptable Objects/PlaytestingSO.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "PlaytestingSO", menuName = "Scriptable Objects/PlaytestingSO")]
+public class PlaytestingSO : ScriptableObject
+{
+    public int money;
+    public int health;
+
+    public float enemySpawnCooldown;
+    public int enemiesToSpawn;
+
+
+}
