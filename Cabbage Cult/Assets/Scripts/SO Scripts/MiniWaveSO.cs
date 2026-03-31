e954c93faf9372250a0430fc4d9544e73c259b0b
Diana <144166059+diana-f-e@users.noreply.github.com>
2026-03-31 08:43:22 -0700

    miniwave

--- Cabbage Cult/Assets/Scripts/SO Scripts/MiniWaveSO.cs
+++ Cabbage Cult/Assets/Scripts/SO Scripts/MiniWaveSO.cs
@@ -5,10 +5,10 @@ public class MiniWaveSO : ScriptableObject
 {
     //different implementations: 
     [Header("how to: 2 construction types - ratio and complex.")]
-    [Header("for ratio, each type should only be listed once, with the corresponding index in")]
+    [Header("- for ratio, each type should only be listed once, with the corresponding index in")]
     [Header("the chances array denoting the chance of the type spawning.")]
-    [Header("for complex, the chances array can be thought of as a count array,")]
-    [Header("where ")]
+    [Header("- for complex, the types array will spawn a number of enemies of that type")]
+    [Header("equal to the corresponding index in the chances array.")]
     public string construction;
     public int count;
     public int delay;
