a240d7a4ded51cb29dcd5296cedb2aad0407e9e2
Diana <144166059+diana-f-e@users.noreply.github.com>
2026-04-02 08:14:04 -0700

    ratio and delay work for wave system

--- Cabbage Cult/Assets/Scripts/SO Scripts/WaveSO.cs
+++ Cabbage Cult/Assets/Scripts/SO Scripts/WaveSO.cs
@@ -3,6 +3,5 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "WaveSO", menuName = "Scriptable Objects/WaveSO")]
 public class WaveSO : ScriptableObject
 {
-    public int delay;
     public MiniWaveSO[] miniWaves;
 }
