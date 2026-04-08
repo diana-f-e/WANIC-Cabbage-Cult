d5e94bb1a4301ff307c1b64821775d5de8f6f94e
Diana <144166059+diana-f-e@users.noreply.github.com>
2026-04-08 08:33:47 -0700

    helmet health addable

--- Cabbage Cult/Assets/Scripts/SO Scripts/EnemySO.cs
+++ Cabbage Cult/Assets/Scripts/SO Scripts/EnemySO.cs
@@ -6,6 +6,7 @@ public class EnemySO : ScriptableObject
     public int enemyDamage;
     public int enemyHealth;
     public float enemySpeed;
+    public int helmetHealth;
     public AudioClip onHurt;
     public AudioClip onDeath;
     public Sprite skin;
